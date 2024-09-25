dc3aa685cdf598320c326831197aaf4786a1f74e
Akshay Utekar <akshayutekar1825@gmail.com>
2024-09-25 22:39:54 +0530

    Add project files.

---
+++ ArmstrongDemo/Model/Armstrong.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongDemo.Model
+{
+    internal class Armstrong
+    {
+        public bool IsArmstrong(int num)
+        {
+            int sum = 0;
+            int temp = num;
+            int digits = num.ToString().Length;
+
+            while (temp > 0)
+            {
+                int remainder = temp % 10;
+                sum += (int)Math.Pow(remainder, digits);
+                temp /= 10;
+            }
+            return sum == num;
+        }
+    }
+}
---
+++ ArmstrongDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using ArmstrongDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Armstrong armstrong = new Armstrong();
+
+            Console.WriteLine("Enter a number :");
+            int number = int.Parse(Console.ReadLine());
+
+            string answer = armstrong.IsArmstrong(number)
+                            ? $"{number} is an Armstrong number."
+                            : $"{number} is not an Armstrong number.";
+
+            Console.WriteLine(answer);
+        }
+    }
+}
